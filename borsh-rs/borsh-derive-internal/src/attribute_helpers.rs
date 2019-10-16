f1a335d3754d78076529ba44b4ca9baa26e176d8
Mikhail <mikhail@nearprotocol.com>
2019-10-16 14:34:53 -0700

    Update dependencies

--- borsh-rs/borsh-derive-internal/src/attribute_helpers.rs
+++ borsh-rs/borsh-derive-internal/src/attribute_helpers.rs
@@ -1,10 +1,11 @@
 use syn::spanned::Spanned;
-use syn::{Attribute, Error, Ident, Meta, NestedMeta};
+use syn::{Attribute, Error, Meta, NestedMeta, Path};
+use quote::ToTokens;
 
 pub fn contains_skip(attrs: &[Attribute]) -> bool {
     for attr in attrs.iter() {
-        if let Ok(Meta::Word(ident)) = attr.parse_meta() {
-            if ident.to_string().as_str() == "borsh_skip" {
+        if let Ok(Meta::Path(path)) = attr.parse_meta() {
+            if path.to_token_stream().to_string().as_str() == "borsh_skip" {
                 return true;
             }
         }
@@ -12,10 +13,10 @@ pub fn contains_skip(attrs: &[Attribute]) -> bool {
     false
 }
 
-pub fn contains_initialize_with(attrs: &[Attribute]) -> syn::Result<Option<Ident>> {
+pub fn contains_initialize_with(attrs: &[Attribute]) -> syn::Result<Option<Path>> {
     for attr in attrs.iter() {
         if let Ok(Meta::List(meta_list)) = attr.parse_meta() {
-            if meta_list.ident.to_string().as_str() == "borsh_init" {
+            if meta_list.path.to_token_stream().to_string().as_str() == "borsh_init" {
                 if meta_list.nested.len() != 1 {
                     return Err(Error::new(
                         meta_list.span(),
@@ -23,8 +24,8 @@ pub fn contains_initialize_with(attrs: &[Attribute]) -> syn::Result<Option<Ident
                     ));
                 }
                 let nested_meta = meta_list.nested.iter().next().unwrap();
-                if let NestedMeta::Meta(Meta::Word(ident)) = nested_meta {
-                    return Ok(Some(ident.clone()));
+                if let NestedMeta::Meta(Meta::Path(path)) = nested_meta {
+                    return Ok(Some(path.clone()));
                 }
             }
         }
