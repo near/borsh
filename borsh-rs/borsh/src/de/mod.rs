3421f6b3e806b9b045c393aae14c1e2e9e843174
Magic_RB <magic_rb@redalder.org>
2020-04-16 23:12:05 +0200

    Implement BorshSerialize and BorshDeserialize for more arrays

--- borsh-rs/borsh/src/de/mod.rs
+++ borsh-rs/borsh/src/de/mod.rs
@@ -443,7 +443,7 @@ macro_rules! impl_arrays {
     };
 }
 
-impl_arrays!(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 32 64 65);
+impl_arrays!(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 64 65 128 256 512 1024 2048);
 
 impl<T> BorshDeserialize for [T; 0]
 where
