8a72299a663d25999dee63f33ec881d325b4d5b6
Serhii Volovyk <sergeyvolovyk@gmail.com>
2020-11-22 16:54:41 +0200

    js build artifacts deleted, prettier and tslint added

--- borsh-ts/test/serialize.test.js
+++ borsh-ts/test/serialize.test.js
@@ -1,5 +1,4 @@
-
-const borsh = require('../../borsh-js/index.js');
+const borsh = require('../lib/index.js');
 
 class Test {
     constructor(x, y, z, q) {
@@ -12,7 +11,20 @@ class Test {
 
 test('serialize object', async () => {
     const value = new Test(255, 20, '123', [1, 2, 3]);
-    const schema = new Map([[Test, {kind: 'struct', fields: [['x', 'U8'], ['y', 'U64'], ['z', 'String'], ['q', [3]]] }]]);
+    const schema = new Map([
+        [
+            Test,
+            {
+                kind: 'struct',
+                fields: [
+                    ['x', 'U8'],
+                    ['y', 'U64'],
+                    ['z', 'String'],
+                    ['q', [3]],
+                ],
+            },
+        ],
+    ]);
     let buf = borsh.serialize(schema, value);
     let new_value = borsh.deserialize(schema, Test, buf);
     expect(new_value.x).toEqual(255);
@@ -20,4 +32,3 @@ test('serialize object', async () => {
     expect(new_value.z).toEqual('123');
     expect(new_value.q).toEqual(new Uint8Array([1, 2, 3]));
 });
-
