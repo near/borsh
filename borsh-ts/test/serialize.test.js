49c5c14577a731baa238ae3fe9352ab591dcdb2f
Serhii Volovyk <sergeyvolovyk@gmail.com>
2020-11-22 19:53:50 +0200

    changes from near-api-js

--- borsh-ts/test/serialize.test.js
+++ borsh-ts/test/serialize.test.js
@@ -1,30 +1,18 @@
 const borsh = require('../lib/index.js');
 
-class Test {
-    constructor(x, y, z, q) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.q = q;
+class Assignable {
+    constructor(properties) {
+        Object.keys(properties).map((key) => {
+            this[key] = properties[key];
+        });
     }
 }
 
+class Test extends Assignable { }
+
 test('serialize object', async () => {
-    const value = new Test(255, 20, '123', [1, 2, 3]);
-    const schema = new Map([
-        [
-            Test,
-            {
-                kind: 'struct',
-                fields: [
-                    ['x', 'U8'],
-                    ['y', 'U64'],
-                    ['z', 'String'],
-                    ['q', [3]],
-                ],
-            },
-        ],
-    ]);
+    const value = new Test({ x: 255, y: 20, z: '123', q: [1, 2, 3] });
+    const schema = new Map([[Test, { kind: 'struct', fields: [['x', 'u8'], ['y', 'u64'], ['z', 'string'], ['q', [3]]] }]]);
     let buf = borsh.serialize(schema, value);
     let new_value = borsh.deserialize(schema, Test, buf);
     expect(new_value.x).toEqual(255);
